ecd075e3e5c9e2a22d80df1ed68cfb7ed3b52ee7
Gryven Alverdo <alfredogryven@gmail.com>
2026-09-19 08:27:09 +0800

    fix: move globals.css to layout to apply styles to all pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import "./globals.css";
+import Link from "next/link";
 import { dict } from "../lib/dictionary";
 
 export default function Portfolio() {
